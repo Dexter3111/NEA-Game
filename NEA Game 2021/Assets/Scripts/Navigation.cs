c1b07256b3e0a7b835f598bf4f61e076f5bcf5ff
Dexter3111 <dobso.d02@elliotthudsoncollege.ac.uk>
2020-09-28 14:46:20 +0100

    28/09/20

---
+++ NEA Game 2021/Assets/Scripts/Navigation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour {
+
+	//Start Button
+	public void ClickedButtonStart(){
+		//Debug.Log ("Start Game Button Clicked");
+		SceneManager.LoadScene(2);
+	}
+	//Tutorial Button
+	public void ClickedButtonTutorial(){
+		//Debug.Log ("Tutorial button clicked");
+		SceneManager.LoadScene (1);
+	}
+	//Controls Button
+	public void ClickedButtonControls(){
+		//Debug.Log("Controls Button Clicked");
+		SceneManager.LoadScene(3);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
+
+	}
+}
