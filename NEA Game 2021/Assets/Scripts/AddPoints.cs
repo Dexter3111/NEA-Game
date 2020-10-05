df1839b7ae540c17287e14a7ef661101ef2bd735
Dexter3111 <dobso.d02@elliotthudsoncollege.ac.uk>
2020-10-05 14:45:05 +0100

    05/10/20 health collect

---
+++ NEA Game 2021/Assets/Scripts/AddPoints.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddPoints : MonoBehaviour {
+
+	public int score;
+	public TextMeshPro scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+
+	//Collectibles
+	void OnCollisionEnter2D(Collision2D collision){
+
+		//Coin collectible
+		if (collision.gameObject.tag == "coin") {
+			//Debug.Log ("Coin");
+			Destroy (collision.gameObject);
+			score += 5;
+			scoreText.text = "Score: " + score.ToString();
+		}
+
+		if (collision.gameObject.tag == "hearts") {
+			Debug.Log ("Heart");
+			Destroy (collision.gameObject);
+
+			//code it so it gives full health when picked up
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
